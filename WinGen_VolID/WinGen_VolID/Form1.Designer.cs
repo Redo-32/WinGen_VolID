e8aeab7e961d400545c52e6eaeb6accc75952853
Redo-32 <142719287+Redo-32@users.noreply.github.com>
2024-11-18 21:14:28 +0100

    Add files via upload

---
+++ WinGen_VolID/WinGen_VolID/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace WinGen_VolID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GenHWID = new System.Windows.Forms.Button();
+            this.HWID = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ChangeHWID = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GenHWID
+            // 
+            this.GenHWID.Location = new System.Drawing.Point(119, 56);
+            this.GenHWID.Name = "GenHWID";
+            this.GenHWID.Size = new System.Drawing.Size(74, 30);
+            this.GenHWID.TabIndex = 0;
+            this.GenHWID.Text = "Generate";
+            this.GenHWID.UseVisualStyleBackColor = true;
+            this.GenHWID.Click += new System.EventHandler(this.GenHWID_Click);
+            // 
+            // HWID
+            // 
+            this.HWID.BackColor = System.Drawing.SystemColors.Window;
+            this.HWID.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HWID.Location = new System.Drawing.Point(20, 27);
+            this.HWID.MaxLength = 10;
+            this.HWID.Name = "HWID";
+            this.HWID.ReadOnly = true;
+            this.HWID.Size = new System.Drawing.Size(174, 23);
+            this.HWID.TabIndex = 1;
+            this.HWID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.HWID);
+            this.groupBox1.Controls.Add(this.ChangeHWID);
+            this.groupBox1.Controls.Add(this.GenHWID);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(214, 97);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Volume ID";
+            // 
+            // ChangeHWID
+            // 
+            this.ChangeHWID.Location = new System.Drawing.Point(20, 56);
+            this.ChangeHWID.Name = "ChangeHWID";
+            this.ChangeHWID.Size = new System.Drawing.Size(74, 30);
+            this.ChangeHWID.TabIndex = 0;
+            this.ChangeHWID.Text = "Change";
+            this.ChangeHWID.UseVisualStyleBackColor = true;
+            this.ChangeHWID.Click += new System.EventHandler(this.ChangeHWID_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 121);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WinOS VolumeID";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GenHWID;
+        private System.Windows.Forms.TextBox HWID;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button ChangeHWID;
+    }
+}
+
---
+++ WinGen_VolID/WinGen_VolID/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinGen_VolID
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GenHWID_Click(object sender, EventArgs e)
+        {
+            string hwid = GenVolID();
+            HWID.Text = hwid;
+        }
+        private string GenVolID()
+        {
+            Random random = new Random();
+            string hexCharacters = "0123456789ABCDEF";
+            string hwid = "";
+
+            for (int i = 0; i < 4; i++)  // Generate 4 characters for the first segment
+            {
+                hwid += hexCharacters[random.Next(hexCharacters.Length)];
+            }
+
+            hwid += "-";
+
+            for (int i = 0; i < 4; i++)  // Generate 4 characters for the second segment
+            {
+                hwid += hexCharacters[random.Next(hexCharacters.Length)];
+            }
+
+            return hwid;
+        }
+
+        private void ChangeHWID_Click(object sender, EventArgs e)
+        {
+ // Validate the HWID format using a regular expression or similar mechanism
+    bool isValidHWID = ValidateHWID(this.TextBox1.Text);
+
+    if (isValidHWID)
+    {
+        // Prepare and start the process
+        var info1 = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            Arguments = "/c Volumeid.exe {this.ComboBox1.SelectedItem} {this.TextBox1.Text}",
+            WindowStyle = ProcessWindowStyle.Hidden,
+            CreateNoWindow = true
+        };
+
+        try
+        {
+            Process.Start(info1);
+            MessageBox.Show("HWID Changed. Please reboot PC to apply it.", "HWID Changed", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error starting process: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+    else
+    {
+        MessageBox.Show("Invalid HWID. Please enter a valid HWID.", "Invalid HWID", MessageBoxButtons.OK, MessageBoxIcon.Error);
+    }
+}
+
+// A method to validate HWID format - implement your own validation logic
+private bool ValidateHWID(string hwid)
+{
+    // Example of a simple validation using a regex pattern
+    var pattern = @"^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$";
+    var regex = new Regex(pattern, RegexOptions.IgnoreCase);
+    return regex.IsMatch(hwid);
+}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Get all drive info
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            // Check if the Volumeid.exe file exists and create it if it doesn't
+            string volumeIdPath = Path.Combine(Application.StartupPath, "Volumeid.exe");
+            if (!File.Exists(volumeIdPath))
+            {
+                File.WriteAllBytes(volumeIdPath, Resources.Volumeid);
+            }
+
+            // Populate ComboBox with drive names
+            foreach (var drive in drives)
+            {
+                if (drive.DriveType == DriveType.Fixed) // Fixed drive type is used for local drives
+                {
+                    // Remove the trailing backslash from the drive name if present
+                    string driveName = drive.Name.TrimEnd(Path.DirectorySeparatorChar);
+                    this.ComboBox1.Items.Add(driveName);
+                }
+            }
+
+            // Set the selected index to the first item in the ComboBox
+            if (this.ComboBox1.Items.Count > 0)
+            {
+                this.ComboBox1.SelectedIndex = 0;
+            }
+        }
+        }
+        }
